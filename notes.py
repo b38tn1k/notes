42da9eebb5d06fd9d101ff636d91bf67c5bef73a
James Carthew <jamesrobertcarthew@gmail.com>
2015-02-04 17:34:27 +1100

    more arguments and slight change to composer algo
    and a lot of tidying

--- notes.py
+++ notes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from os import system
 import argparse
 from midiutil.MidiFile import MIDIFile
 import numpy
@@ -23,6 +22,25 @@ from scales import WesternScale
 # is used. Additional modes would allow for an
 # extended range of expression
 ##################################################
+# TODO: BEATIDI creates melodies using the
+# procedural generation engine. KICK and SNARE
+# patterns are created using a 2 channel version
+# of procedural generation, hi hat is generated
+# separately, using a single channel and more
+# iterations
+#
+# TODO: MELIDI is a command line version of notes
+# - the western music scale class must be in the
+# same file
+#
+# TODO: the beat reset needs to be carried across
+# loops (currently it is resetting)
+#
+# TODO: convert scale generator into a python
+# generator
+#
+# TODO: look into how midiutil handles 1/2, 1/4,
+# 1/16, etc beats
 
 parser = argparse.ArgumentParser(description='NOTES procedurally generates small MIDI files that can be looped')
 parser.add_argument('intervals', metavar='N', type=int, nargs='+',
@@ -35,17 +53,18 @@ parser.add_argument('-ts', '--meter',
                     help="set the number of beats in a measure default 4", required=False)
 parser.add_argument('-i', '--iterations',
                     help='Set the number of procedural iterations',  required=True)
-parser.add_argument('-o', '--output', help='set the name of the outputted MIDI file (no extension required)', required=False)
+parser.add_argument('-o', '--output',
+                    help='set the name of the outputted MIDI file (no extension required)', required=False)
+parser.add_argument('-v', '--verbose', help='toggle verbose mode', dest='verbose',  action='store_true', required=False)
 
 args = vars(parser.parse_args())
 
 def main():
 
-    # Song Seed
     if args['tonality']:
         tonality = args['tonality']
     else:
-        tonality = "minor"
+        tonality = 'minor'
 
     if args['intervals']:
         intervals = args['intervals']
@@ -68,10 +87,14 @@ def main():
     melody_iteration = int(args['iterations'])
 
     if args['output']:
-        track_name = str(args['output'])
+        track_name = str(args['output']) + ".mid"
     else:
         track_name = "Intervals_" + str(track_interval_one) + "_" + str(track_interval_two) + "_Iterations_" + str(melody_iteration) + "_Tonality_" + tonality + ".mid"
 
+    if args['verbose']:
+        verbose = True
+    else:
+        verbose = False
     beats_in_loop = meter*loop_length - 1
     inverse_melody_iteration = melody_iteration
 
@@ -92,7 +115,6 @@ def main():
     # some of these fields could be approached at a
     # later date to achieve a more humanised feel (for
     # example velocity) but for now I am lazy
-
     track_index = 0
     time_index = 0
     track_channel = 0
@@ -104,9 +126,10 @@ def main():
     # Composition Logic
     while melody_iteration > 0:
         if process_loop[beat] > 0.0:
-            print "Switch at Iteration: " + str(melody_iteration) + " Beat: " + str(beat) \
-                + " Interval: " + str(process_interval) + " Note Value: " + str(process_loop[beat]) \
-                  + " Trigger Value: " + str(process_loop[beat])
+            if verbose:
+                print "Switch at Iteration: " + str(melody_iteration) + " Beat: " + str(beat) \
+                    + " Interval: " + str(process_interval) + " Note Value: " + str(process_loop[beat]) \
+                      + " Trigger Value: " + str(process_loop[beat])
             if process_interval == track_interval_one:
                 process_interval = track_interval_two
             else:
@@ -115,16 +138,16 @@ def main():
         track_handle.addNote(track_index, track_channel, current_note, beat, note_duration, track_volume)
         current_note = scale.next_pitch(current_note)
         beat += process_interval
-        # TODO: the beat reset needs to be carried across loops (currently it is resetting)
-        # TODO: convert scale generator into a proper generator
         if beat > beats_in_loop:
-            print 'Reached End of Loop at: ' + str(beat)
+            if verbose:
+                print 'Reached End of Loop at: ' + str(beat)
             melody_iteration -= 1
-            beat -= beats_in_loop
-            print 'Beat reset to: ' + str(beat)
-        print "Iteration: " + str(inverse_melody_iteration-melody_iteration) + " Beat: " + str(beat)
-        print process_loop
-
+            beat -= beats_in_loop + 1       # The +1 returns the cursor to beat 1 rather than beat 0 (out of bounds)
+            if verbose:
+                print 'Beat reset to: ' + str(beat)
+        if verbose:
+            print "Iteration: " + str(inverse_melody_iteration-melody_iteration) + " Beat: " + str(beat)
+            print process_loop
 
     # MIDI print
     bin_file = open(track_name, 'wb')
