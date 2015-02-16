eb83e1439daa85bffe1f35f2d31fbac17d68624d
James Carthew <jamesrobertcarthew@gmail.com>
2015-02-17 09:46:25 +1100

    some dumb stuff in scales

--- scales.py
+++ scales.py
@@ -14,6 +14,7 @@ __author__ = 'jamescarthew'
 # 0  2  3  5  7  8  10 12
 ##################################################
 
+
 class NoteMap(object):
     def __init__(self, root_note, tonality):
         self.root_note = root_note
@@ -21,6 +22,7 @@ class NoteMap(object):
         self.tones = ''
         self.semis = ''
         self.octave = ''
+        self.tonality = tonality
         if tonality == "major":
             self.tones = [0, 2, 5, 7, 9, 12, 14, 17, 21, 24]
             self.semis = [4, 11, 16, 23]
@@ -43,11 +45,12 @@ class NoteMap(object):
                 next_pitch += 1
             if current_interval in self.tones:
                 next_pitch += 2
-            if next_pitch > (self.root_note + 24):            # Max Pitch = 255 however super high notes sound bad generally
+            if next_pitch > (self.root_note + 24):
+                # Max Pitch = 255 however super high notes sound bad generally
                 next_pitch -= 24
             return next_pitch
 
-        if self.octave == 2:
+        if self.tonality == "drums":
             if current_pitch == 38:
                 current_pitch = 36
             else:
